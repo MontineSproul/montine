715205fffc710617a70c3c35f0df1b50b66db963
Montine Sproul <montinesproul@Montines-MBP.lan>
2020-03-11 20:32:10 -0400

    add resume and other stuff

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php include('header.php'); ?>
-
-        <div class="header">
-            <div class="border">
-            <h1>Montine Sproul</h1>
-            <p>On this page I will indroduce myself to the world of Web Developement!</p>
-            </div>
-        </div>
-</main>
+<div class="header">
+    <div class="border">
+        <h1>Montine Sproul</h1>
+        <p>On this page I will introduce myself to the world of Web Development!</p>
+    </div>
+</div>
 <?php include('footer.php'); ?>
-    </body>
-</html>
--- resume.php
+++ resume.php
@@ -1,6 +1,4 @@
 <?php include('header.php'); ?>
-         <h1>Montine Sproul Resume</h1>
-    </main>
-</body>
-</main>
-</html>
+<div class="canva-embed" data-design-id="DAB1vhKCcO8" data-height-ratio="1.2941" style="padding:129.4118% 5px 5px 5px;background:rgba(0,0,0,0.03);border-radius:8px;"></div>
+<script async src="https:&#x2F;&#x2F;sdk.canva.com&#x2F;v1&#x2F;embed.js"></script>
+<?php include('footer.php'); ?>
